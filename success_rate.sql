494951e80af6d00335c6d7368102e7b1c7dfe3d4
Ramon <ramonvkoppe@gmail.com>
2025-03-24 10:22:59 -0300

    Delete success_rate.sql

--- success_rate.sql
+++
@@ -1,6 +0,0 @@
-SELECT Industry, Required_Experience, COUNT(*) AS Total_Applications, COUNT(CASE WHEN ja.Status = 'Hired' THEN 1 END) AS Hired,
-	   CONCAT(ROUND((COUNT(CASE WHEN ja.Status = 'Hired' THEN 1 END) / COUNT(*)) * 100, 1), "%") AS Success_Rate
-FROM job_postings AS jp
-INNER JOIN job_applications AS ja ON jp.Job_ID = ja.Job_ID
-GROUP BY Industry, Required_Experience
-ORDER BY ROUND((COUNT(CASE WHEN ja.Status = 'Hired' THEN 1 END) / COUNT(*)) * 100, 1) DESC;
